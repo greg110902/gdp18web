30604729094fa10a70fe165e3a88f594875a32c4
Greg Garcia Humphreys <gwgh1g21@soton.ac.uk>
2025-02-03 13:27:50 +0000

    added conditional to lat long

--- app/page.js
+++ app/page.js
@@ -158,7 +158,7 @@ export default function Home() {
           <tr className="border">
             <td class="tg-baqh">GPS Coordinates</td>
             <td class="tg-baqh" className="flex justify-center align-middle">
-              redact
+              {dataLoaded ? `${currentPos.lat}, ${currentPos.long}` : <></>}
             </td>
           </tr>
         </tbody>
