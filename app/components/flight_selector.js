8ee48302babd7768879077dc6129dc22a84ce726
Greg Garcia Humphreys <gwgh1g21@soton.ac.uk>
2025-02-11 15:33:14 +0000

    moved flight selector into condition

--- app/components/flight_selector.js
+++ app/components/flight_selector.js
@@ -7,43 +7,44 @@ export default function FlightSelector(
   flightid
 ) {
   const [flightID, setFlightID] = useState();
+  const [idLoaded, setIDLoaded] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-    
-  })
-  if (flightID)
+    setFlightID(flightid);
+    setIDLoaded(true);
+  });
 
-  setFlightID(flightid);
+  if (idLoaded) {
+    console.log(`flightID ${flightid}`);
 
-  console.log(`flightID ${flightid}`);
+    function incrementID() {
+      setFlightID(flightID + 1);
+      router.push(`/maps/${flightID}`);
+    }
+    function decrementID() {
+      setFlightID(flightID - 1);
+      router.push(`/maps/${flightID}`);
+    }
 
-  function incrementID() {
-    setFlightID(flightID + 1);
-    router.push(`/maps/${flightID}`);
+    var lat1 = (penultimatePoint["lat"] * Math.PI) / 180;
+    var lat2 = (ultimatePoint["lat"] * Math.PI) / 180;
+    var long1 = (penultimatePoint["long"] * Math.PI) / 180;
+    var long2 = (ultimatePoint["long"] * Math.PI) / 180;
+
+    var d_phi = lat2 - lat1;
+    var d_lambda = long2 - long1;
+
+    return (
+      <div>
+        <button className="btn" onClick={decrementID()}>
+          {"<"}
+        </button>
+        <>{flightID}</>
+        <button className="btn" onClick={incrementID()}>
+          {">"}
+        </button>
+      </div>
+    );
   }
-  function decrementID() {
-    setFlightID(flightID - 1);
-    router.push(`/maps/${flightID}`);
-  }
-
-  var lat1 = (penultimatePoint["lat"] * Math.PI) / 180;
-  var lat2 = (ultimatePoint["lat"] * Math.PI) / 180;
-  var long1 = (penultimatePoint["long"] * Math.PI) / 180;
-  var long2 = (ultimatePoint["long"] * Math.PI) / 180;
-
-  var d_phi = lat2 - lat1;
-  var d_lambda = long2 - long1;
-
-  return (
-    <div>
-      <button className="btn" onClick={decrementID()}>
-        {"<"}
-      </button>
-      <>{flightID}</>
-      <button className="btn" onClick={incrementID()}>
-        {">"}
-      </button>
-    </div>
-  );
 }
