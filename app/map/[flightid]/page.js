a85934c6672ec62de4dba5560646ffd140e8fa1b
greg110902 <gwgh1g21@soton.ac.uk>
2025-04-10 17:12:29 +0100

    added callout

--- app/map/[flightid]/page.js
+++ app/map/[flightid]/page.js
@@ -11,6 +11,7 @@ import ac_marker from "/public/airplane-svgrepo-com.png";
 import useRouter from "next/navigation";
 
 export default function Home() {
+  // Init states and hooks
   const [map, setGMap] = React.useState(null);
   const [width, setWidth] = React.useState();
   const [lastItem, setLastItem] = React.useState(0);
@@ -27,7 +28,6 @@ export default function Home() {
   const [imsLoaded, setImsLoaded] = React.useState(false);
   const [dataLoaded, setDataLoaded] = React.useState(false);
   const [FlightID, setFlightID] = React.useState(0);
-  //const [height, setHeight] = React.useState()
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyAs91rb9-ScOhp1A6CyZBbpR4LrUdtzeGo",
@@ -35,39 +35,52 @@ export default function Home() {
   const R = 6371e3;
   const { flightid } = useParams();
 
-  console.log(flightid);
-  console.log(`Converted ${Number(flightid[0])}`);
   useEffect(() => {
+    // useEffect runs while the page is open
     const fetchData = async () => {
+      // Collect ground track etc data from database
       const d = await fetch("https://telemetry-worker.gwgh1g21.workers.dev/");
 
       setData(await d.json());
       setDataLoaded(true);
     };
 
+    // Collects the data from the spotted locations database
     const fetchIms = async () => {
       const data = await fetch("https://img-worker.gwgh1g21.workers.dev/");
       setIms(await data.json());
       setImsLoaded(true);
     };
+
+    // Set width of map to the screen width
     setWidth(window.screen.width);
 
+    // Code within setTimeout() runs every 1000ms to save performance + reduce requests
     setTimeout(() => {
+      // Fetch ground track and spotted locations data
       fetchData();
       fetchIms();
+
       if (dataLoaded) {
         setLastItem(data.length - 1);
         var FlightPath = [];
+
+        // Populate array with ground track data
         data.forEach((element) => {
           FlightPath.push({ lat: element["lat"], lng: element["long"] });
         });
+
+        // Ground track
         const line = new google.maps.Polyline({
           path: FlightPath,
           strokeColor: "#FC0303",
         });
 
+        line.setMap(map);
+
+        // Creates markers for the spotted locations which shows the images
         ims.forEach((loc) => {
-          console.log(loc.img_url);
+          console.log(`URL ${loc.img_url}`);
           const m = new google.maps.Marker({
             position: { lat: loc.lat, lng: lat.long },
             onClick: document.getElementById(loc.img_url).showModal(),
@@ -75,7 +88,7 @@ export default function Home() {
           m.setMap(map);
         });
 
-        line.setMap(map);
+        // Update the data table
         setAlt(data[lastItem]["alt"]);
         setCurrentPos({
           lat: data[lastItem]["lat"],
@@ -84,9 +97,8 @@ export default function Home() {
         setHeading(data[lastItem]["head"]);
       }
 
-      console.log("CALCULATING LATS");
-
       if (dataLoaded) {
+        // Calculates the distance between the points
         var lat1 = (data[lastItem - 1]["lat"] * Math.PI) / 180;
         var lat2 = (data[lastItem]["lat"] * Math.PI) / 180;
         var long1 = (data[lastItem - 1]["long"] * Math.PI) / 180;
@@ -143,11 +155,6 @@ export default function Home() {
     map.setCenter(map.getCenter());
     console.log(JSON.stringify(currentPos));
   }
-  //if (isLoaded && dataLoaded && data == null) {
-  //redirect("/flight-not-initiated", "replace");
-  //}
-
-  console.log(`FlightID before component ${flightid[0]}`);
 
   return isLoaded && dataLoaded ? (
     <>
